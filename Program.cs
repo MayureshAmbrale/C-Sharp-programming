daa14662a928bde082c20e09c8e938caf1f9f366
Mayuresh Ambrale <167439890+MayureshAmbrale@users.noreply.github.com>
2024-07-31 20:26:05 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,36 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-class Addition
+namespace Program4
 {
-    private int No1, No2;
-    public Addition(int A, int B)
-    {
-        this.No1 = A;
-        this.No2 = B;
-    }
-    public int Add()
-    {
-        int Ans = 0;
-        Ans = No1 + No2;
-        return Ans;
-    }
-
-}
-namespace Program2
-{
-    internal class Program
-    {
+    class Program
+    {   
+        public static bool CheckEvenOdd(int no)
+        {
+            bool bFlag = false;
+            if (no % 2 == 0)
+            {
+                bFlag = true;
+            }
+           
+            return bFlag;
+        }
         static void Main(string[] args)
         {
-            int Ret = 0;
-            Console.WriteLine("Enter number 1");
-            int n1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter number 2");
-            int n2 = Convert.ToInt32(Console.ReadLine());
-            Addition aobj = new Addition(n1, n2);
-            Ret = aobj.Add();
-            Console.WriteLine(Ret);
+            Console.WriteLine("Enter number :");
+            int No1=Convert.ToInt32(Console.ReadLine());
+            bool Ret = false;
+            Ret = CheckEvenOdd(No1);
+            if(Ret==true)
+            {
+                Console.WriteLine("Number is even");
+            }
+            else
+            {
+                Console.WriteLine("Number is not even");
+            }
+
         }
     }
 }
