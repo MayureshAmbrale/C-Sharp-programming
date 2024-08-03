098f75f27af248fc2e4f382d121b16b0913180f8
Mayuresh Ambrale <167439890+MayureshAmbrale@users.noreply.github.com>
2024-08-03 17:18:57 +0530

    Add files via upload

---
+++ Program13.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program12
+{
+    internal class Program
+    {
+        public static long fact = 1;
+        static long Factorial(int n)
+        {
+           
+            if(n!=0)
+            {
+                fact = fact * n;
+                
+                Factorial(n - 1);
+                return fact;
+
+            }
+
+            return fact;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number :");
+            int no = Convert.ToInt32(Console.ReadLine());
+
+            long Ret = 0;
+            Ret = Factorial(no);
+            Console.WriteLine("Factorial is : " + Ret);
+        }
+    }
+}
