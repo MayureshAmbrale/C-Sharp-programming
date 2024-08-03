5d29bce16b6d48a382fe9714d6822eb8e39a78f1
Mayuresh Ambrale <167439890+MayureshAmbrale@users.noreply.github.com>
2024-08-03 17:19:22 +0530

    Add files via upload

---
+++ Program14.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program14
+{
+    class Digits
+    {
+        int No;
+        public Digits(int no)
+        {
+            this.No = no;
+        }
+        public void DisplayDigits()
+        {
+            int d =0;
+            while(No>0)
+            {
+                d = No % 10;
+                Console.WriteLine(d);
+                No = No / 10;
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number :");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Digits dg = new Digits(n);
+            dg.DisplayDigits();
+        }
+    }
+}
