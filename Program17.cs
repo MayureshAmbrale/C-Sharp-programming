f815d672e6f20c4fdd3a98545b1a74fcfa123fbc
Mayuresh Ambrale <167439890+MayureshAmbrale@users.noreply.github.com>
2024-08-03 17:20:30 +0530

    Add files via upload

---
+++ Program17.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program14
+{
+    class Digits
+    {
+        int No;
+
+        public Digits(int no)
+        {
+            this.No = no;
+        }
+        public int CountOddDigits()
+        {
+            int iCnt = 0;
+            int d = 0;
+            while (No > 0)
+            {
+                d = No % 10;
+                if (d % 2 != 0)
+                    iCnt++;
+                No = No / 10;
+            }
+            return iCnt;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number :");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int Ret = 0;
+            Digits dg = new Digits(n);
+            Ret = dg.CountOddDigits();
+
+            Console.WriteLine("Total number of odd digits are :" + Ret);
+        }
+    }
+}
