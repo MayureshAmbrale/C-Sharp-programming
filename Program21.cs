7905da3a6b79897335bc13fbeeb227d50250b4bc
Mayuresh Ambrale <167439890+MayureshAmbrale@users.noreply.github.com>
2024-08-04 23:37:48 +0530

    Add files via upload

---
+++ Program21.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program20
+{
+    public class Pattern
+    {
+        public int iRow;
+        public int iCol;
+
+        public Pattern(int i, int j)
+        {
+            this.iRow = i;
+            this.iCol = j;
+        }
+        public void Display()
+        {
+            int i = 0, j = 0;
+            for (i = 0; i < iRow; i++)
+            {
+                for (j = 1; j <= iCol; j++)
+                {
+                    if(j % 2==0)
+                    Console.Write("* ");
+                    else
+                        Console.Write("# ");
+
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number of rows :");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter number of columns :");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+
+            Pattern p = new Pattern(n1, n2);
+
+            p.Display();
+        }
+    }
+}
