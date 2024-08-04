d6c32a03c448e8b52c4b8e3305e307c6995b81cb
Mayuresh Ambrale <167439890+MayureshAmbrale@users.noreply.github.com>
2024-08-04 23:36:45 +0530

    Add files via upload

---
+++ Program19.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program14
+{
+    class Digits
+    {
+        int No;
+
+        public Digits(int no)
+        {
+            this.No = no;
+        }
+        public int Palendroam()
+        {
+            int Rev = 0;
+            int d = 0;
+            while (No > 0)
+            {
+                d = No % 10;
+                Rev = (Rev *10) + d;
+                No = No / 10;
+            }
+            return Rev;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number :");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int Ret = 0;
+            Digits dg = new Digits(n);
+            Ret = dg.Palendroam();
+
+            Console.WriteLine("Reverse number  is :" + Ret);
+        }
+    }
+}
