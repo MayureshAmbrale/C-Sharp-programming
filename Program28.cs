838c0b99a101cea05a8b07ff80167f4e7ebc584f
Mayuresh Ambrale <167439890+MayureshAmbrale@users.noreply.github.com>
2024-08-19 11:43:09 +0530

    Add files via upload

---
+++ Program28.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program25
+{
+    class Array
+    {
+        public void DisplayOdd(int[] arr)
+        {
+            int sum = 0;
+            Console.WriteLine("Elements of array are :");
+            foreach (int j in arr)
+            {
+                if (j % 2 != 0)
+                {
+                    Console.WriteLine(j + " ");
+                }
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ret = 0;
+            Console.WriteLine("Enter total number in array :");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[n];
+
+            Console.WriteLine("Enter elements in array :");
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Array a = new Array();
+            a.DisplayOdd(arr);
+
+
+
+
+        }
+    }
+}
