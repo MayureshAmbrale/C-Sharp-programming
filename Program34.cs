bf020ff9917d323ea9315354265061be762bfd6e
Mayuresh Ambrale <167439890+MayureshAmbrale@users.noreply.github.com>
2024-08-22 19:08:43 +0530

    Add files via upload

---
+++ Program34.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program34
+{
+    internal class Program
+    {
+        public static bool CheckChar(char c)
+        {
+            bool bFlag = false;
+
+            if(c>='A' && c<='Z')
+            {
+                bFlag = true;
+            }
+            return bFlag;
+        }
+        static void Main(string[] args)
+        {
+            bool bRet = false;
+            Console.WriteLine("Enter character :");
+            char ch = Convert.ToChar(Console.ReadLine());
+
+            bRet = CheckChar(ch);
+            if(bRet==true)
+            {
+                Console.WriteLine("Character is Capitai ");
+            }
+            else
+            {
+                Console.WriteLine("Character is not Capital");
+            }
+
+
+        }
+    }
+}
